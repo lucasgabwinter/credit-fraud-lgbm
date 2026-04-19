9626bab145b7ea0505a45f0ecc330da26531f79c
Lucas Winter <lucasgabwinter@gmail.com>
2026-04-19 00:22:28 -0300

    build: arquivo yaml validado

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from src.predict import prever_fraude
 from src.schemas import OprInput
@@ -5,27 +6,36 @@ from src.schemas import OprInput
 MODEL_VERSION = "v1"
 
 def lambda_handler(event, context):
+    resultados = []
     try:
-        transaction_id = event["transaction_id"]
-        features = event["features"]
+        for record in event['Records']:
+            body = json.loads(record['body'])
+            transaction_id = body["transaction_id"]
+            features = body["features"]
 
-        # Validação com Pydantic
-        opr = OprInput(**features)
-        dados_validados = opr.model_dump()
+            # Validação com Pydantic
+            opr = OprInput(**features)
+            dados_validados = opr.model_dump()
 
-        # Predição
-        resultado = prever_fraude(dados_validados)
+            # Predição
+            resultado = prever_fraude(dados_validados)
 
-        # Resposta padronizada
+            resultados.append({
+                "transaction_id": transaction_id,
+                "fraud_score": resultado["prob_fraude"],
+                "prediction": resultado["resultado"],
+                "model_version": MODEL_VERSION,
+                "processed_at": datetime.now().isoformat()
+            })
+
+        # Resposta 
         return {
-            "transaction_id": transaction_id,
-            "fraud_score": resultado["prob_fraude"],
-            "prediction": resultado["resultado"],
-            "model_version": MODEL_VERSION,
-            "processed_at": datetime.now().isoformat()
+            "resultado": "success",
+            "results": resultados,
         }
 
     except Exception as e:
         return {
+            "status": "error",
             "error": str(e)
         }
