8c18e398f0069cb92f9b0d6367941930a43dcfdd
Lucas Winter <lucasgabwinter@gmail.com>
2026-04-19 09:18:15 -0300

    fix: remove o try do lambda handler

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -7,35 +7,28 @@ MODEL_VERSION = "v1"
 
 def lambda_handler(event, context):
     resultados = []
-    try:
-        for record in event['Records']:
-            body = json.loads(record['body'])
-            transaction_id = body["transaction_id"]
-            features = body["features"]
+    for record in event['Records']:
+        body = json.loads(record['body'])
+        transaction_id = body["transaction_id"]
+        features = body["features"]
 
-            # Validação com Pydantic
-            opr = OprInput(**features)
-            dados_validados = opr.model_dump()
+        # Validação com Pydantic
+        opr = OprInput(**features)
+        dados_validados = opr.model_dump()
 
-            # Predição
-            resultado = prever_fraude(dados_validados)
+        # Predição
+        resultado = prever_fraude(dados_validados)
 
-            resultados.append({
-                "transaction_id": transaction_id,
-                "fraud_score": resultado["prob_fraude"],
-                "prediction": resultado["resultado"],
-                "model_version": MODEL_VERSION,
-                "processed_at": datetime.now().isoformat()
-            })
+        resultados.append({
+            "transaction_id": transaction_id,
+            "fraud_score": resultado["prob_fraude"],
+            "prediction": resultado["resultado"],
+            "model_version": MODEL_VERSION,
+            "processed_at": datetime.now().isoformat()
+        })
 
-        # Resposta 
-        return {
-            "resultado": "success",
-            "results": resultados,
-        }
-
-    except Exception as e:
-        return {
-            "status": "error",
-            "error": str(e)
-        }
+    # Resposta 
+    return {
+        "resultado": "success",
+        "results": resultados,
+    }
